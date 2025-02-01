f718511d27a8ac7b10e37a5eab22ef12182932a5
mohammed kelany <102394768+mohammedelsayed17@users.noreply.github.com>
2025-02-01 04:17:23 +0200

    main test class

---
+++ main.cpp
@@ -0,0 +1,71 @@
+// #include "Account.h"
+#include "Account.cpp"
+#include "Client.cpp"
+#include "Bank.cpp"
+// #include "SpecialAccount.h"
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    // int x;
+    // double y;
+    // cout << "enter your account num:" << endl;
+    // cin >> x;
+    // cout << "enter your balance: " << endl;
+    // cin >> y;
+    // Account s1(x, y);
+    // int z;
+    // double t;
+    // cout << "choice your process "
+    //         "\n"
+    //         "1-withdraw"
+    //         "\n"
+    //         "2-deposite"
+    //      << endl;
+    // cout << "enter your choice :" << endl;
+    // cin >> z;
+    // if (z == 1)
+    // {
+    //     cout << "enter your withdraw amount: " << endl;
+    //     cin >> t;
+    //     s1.withdraw(t);
+    // }
+    // else if (z == 2)
+    // {
+    //     cout << "enter your deposit amount: " << endl;
+    //     cin >> t;
+    //     s1.deposit(t);
+    // }
+    // else
+    // {
+    //     cout << "invalide process" << endl;
+    // }
+
+    // Account *ptr;
+    // Account s1(3333, 500);
+    // Client c("mohammed", "00000000", "kafer eldware", "01285", s1);
+    // SpecialAccount s(444, 700);
+    // ptr = &s;
+    // ptr->withdraw(1000);
+    // ptr = &s1;
+    //  s1.getBalance();
+    // cout << s1.getBalance() << endl;
+    // c.display();
+    // Account acc1(12345, 5000);
+    // Client c1("John Doe", "12345678901234", "123 Main St", "0123456789", acc1);
+    // c1.account.withdraw(6000);
+    // c1.display();
+    // Account acc2(67890, 10000);
+    // CommercialClient c2("TechCorp Ltd.", "456 Business Rd", "0987654321", acc2, "COM123456");
+    // c2.diplay();
+    // Bank myBank("XYZ Bank", "123 Bank Street", "0123456789");
+    // Account acc1(1001, 5000.0);
+    // SpecialAccount acc2(2002, 10000.0);
+    // CommercialClient client2("Company XYZ", "Office 123", "555-6789", acc2, "32222");
+    // Client client1("John Doe", "12345678901234", "Downtown", "555-1234", acc1);
+    // myBank.add_Clients(client1, acc1);
+    // myBank.add_Clients(client2, acc2);
+    // myBank.display();
+    return 0;
+}
